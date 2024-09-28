b15debe8dd3d3479de2ac84efc05025bf488e82d
GierAffe <daniel@flyingRig>
2024-09-28 20:29:37 +0200

    Change alpha theme to 'theta'

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -3,7 +3,7 @@ return {
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 
 	config = function()
-		local startify = require("alpha.themes.startify")
+		local startify = require("alpha.themes.theta")
       startify.file_icons.provider = "devicons"
       require("alpha").setup(startify.config)
 	end,
