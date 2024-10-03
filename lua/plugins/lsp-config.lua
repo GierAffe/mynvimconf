0e012b22b6e670c54191c017c4a0a52ac30a9d01
GierAffe <daniel@flyingRig>
2024-10-03 13:48:11 +0200

    Remove LSP and Formating for ASM

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
       "williamboman/mason-lspconfig.nvim",
       config = function()
          require("mason-lspconfig").setup({
-            ensure_installed = { "lua_ls", "clangd", "asm_lsp" },
+            ensure_installed = { "lua_ls", "clangd" },
          })
       end,
    },
