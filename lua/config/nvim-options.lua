c2e18c099f06d9f9941ed8a4eb7feb34f00c2b87
GierAffe <daniel@flyingRig>
2024-10-07 00:29:13 +0200

    Update basic nvim formating and general UI behavior

--- lua/config/nvim-options.lua
+++ lua/config/nvim-options.lua
@@ -1,12 +1,35 @@
--- Tabs
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=3")
-vim.cmd("set softtabstop=3")
-vim.cmd("set shiftwidth=3")
+-- -- Tab settings
+vim.o.tabstop = 3
+vim.bo.tabstop = 3
+vim.o.softtabstop = 3
+vim.o.shiftround = true
 
--- Line Numbers
-vim.cmd("set number")
-vim.cmd("set relativenumber")
+-- Length of indentation with >> <<
+vim.o.shiftwidth = 3
+vim.bo.shiftwidth = 3
+
+-- Use spaces instead of tabs
+vim.o.expandtab = true
+vim.bo.expandtab = true
+
+-- Display realtive line numbers
+vim.wo.number = true
+vim.wo.relativenumber = true
+
+-- Preserve 8 lines around cursor when moving with jkhl
+vim.o.scrolloff = 8
+vim.o.sidescrolloff = 8
+
+-- Highlight current line
+vim.wo.cursorline = true
+
+-- Show left gutter signs
+--vim.wo.signcolumn = "yes"
+
+-- Right reference line
+--vim.wo.colorcolumn = "160"
+
+
 
 vim.g.mapleader = " "
 
